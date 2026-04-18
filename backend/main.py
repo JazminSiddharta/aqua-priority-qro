ea6f7125cc4bcae3609fa41e0af4f940c2d3c4c5
mocelo14-hub <mocelo14@alumnos.uaq.mx>
2026-04-17 21:27:43 -0600

    H3 endpoint reportes funcionando en main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Reporte(BaseModel):
+    tipo: str
+    zona_tipo: str
 
 @app.get("/health")
 async def health():
     return {"status":"ok"}
+
+@app.post("/reportes")
+async def crear_reporte(data: Reporte):
+
+    prioridad = "alta"
+
+    return {
+        "tipo": data.tipo,
+        "zona": data.zona_tipo,
+        "prioridad": prioridad
+    }
